6662f6f44852d48a7d18f1415b18244f405f61fa
Gopal <Gopal1409@gmail.com>
2021-08-18 05:43:17 -0700

    terraform provider

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "myec2vm" {
-  ami = data.aws_ami.amznlinux1.id
+  ami = data.aws_ami.amznlinux2.id
   instance_type = var.instance_type
   user_data = file("${path.module}/app1-install.sh")
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-http.id]
