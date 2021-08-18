c97b1c97f4f8ab7f5a1b7cca28ae6f0083da2c41
Gopal <Gopal1409@gmail.com>
2021-08-18 05:57:34 -0700

    terraform provider

--- ec2-ami.tf
+++ ec2-ami.tf
@@ -1,22 +1,15 @@
 data "aws_ami" "amznlinux2" {
-    most_recent = true #the updated image
-    owners = ["amazon"]
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*"]
-    }
-    filter {
-      name = "root-device-type"
-      values = ["ebs"]
-    }
+ most_recent = true
 
-    filter {
-        name = "virtualization-type"
-        values = ["ebs"]
-    }
-    filter {
-        name = "architecture"
-        values = ["x86_64"]
-    }
 
+ filter {
+   name   = "owner-alias"
+   values = ["amazon"]
+ }
+
+
+ filter {
+   name   = "name"
+   values = ["amzn2-ami-hvm*"]
+ }
 }
