0116ce4b2303eb724d8318a575db564ff98515ac
Gopal <Gopal1409@gmail.com>
2021-08-18 05:41:36 -0700

    terraform provider

--- securitygroup.tf
+++ securitygroup.tf
@@ -57,7 +57,7 @@ resource "aws_security_group" "vpc-http" {
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
+
     }
 
   tags = {
