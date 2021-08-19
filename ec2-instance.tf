0249bde956abd8c4a49ebbdb501142ec9c626c1a
Gopal <Gopal1409@gmail.com>
2021-08-19 02:46:44 -0700

    terraform with for_each

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,3 +1,9 @@
+data "aws_availability_zones" "my_azones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 resource "aws_instance" "myec2vm" {
   ami = data.aws_ami.amznlinux2.id
   #instance_type = var.instance_type
@@ -5,8 +11,9 @@ resource "aws_instance" "myec2vm" {
   instance_type = var.instance_type_map["qa"] #map
   user_data = file("${path.module}/app1-install.sh")
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-http.id]
-  count = 2
+  for_each = toset(data.aws_availability_zones.my_azones.names)
+  availability_zone = each.key #list of items. 
   tags = {
-    "Name" = "gopal-ec2-${count.index}"
+    "Name" = "gopal-ec2-${each.value}"
   }
 }
