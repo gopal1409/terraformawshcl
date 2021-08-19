a5b5c191942c263211fd46a37023168f5ae4259e
Gopal <Gopal1409@gmail.com>
2021-08-19 05:00:43 -0700

    terraform tf state

--- version.tf
+++ version.tf
@@ -5,8 +5,15 @@ terraform {
       version = ">= 2.7.0"
       source = "hashicorp/aws"
     }
+    
   }
+  backend "s3" {
+  bucket = "terraform-state-gopal"
+  key = "/dev/terraform.tfstate"
+  region = "ap-northeast-2"
+ }
 }
+#adding backend s3 bucket for remote state storage
 
 #Provider Block
 provider "aws"{
