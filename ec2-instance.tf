c7af3f31c4b8a7a50c96d9e2e1f1384df2a0e6a8
Gopal <Gopal1409@gmail.com>
2021-08-19 03:24:20 -0700

    instance type supported or unsupported

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -5,7 +5,8 @@ resource "aws_instance" "myec2vm" {
   instance_type = var.instance_type_map["qa"] #map
   user_data = file("${path.module}/app1-install.sh")
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-http.id]
-  for_each = toset(keys({for az, details in data.aws_ec2_instance_type_offering.my_inst_type: az => details.instance_type if(details.instance_type) !=0)})
+  for_each = toset(keys({for az, details in data.aws_ec2_instance_type_offering.my_inst_type:
+   az => details.instance_type if length(details.instance_type) !=0}))
   availability_zone = each.key #list of items. 
   tags = {
     "Name" = "gopal-ec2-${each.key}"
